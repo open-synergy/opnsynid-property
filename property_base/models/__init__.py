677e2335138aaecfd9f11308fbae7fd771967ba2
Andhitia Rama <andhitia.r@gmail.com>
2019-07-10 10:34:50 +0700

    [8.0.2.0.0] property_base

--- property_base/models/__init__.py
+++ property_base/models/__init__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
-        property_object,
-        property_building,
-        property_land,
-        property_floor,
-        property_room)
+    property_object_type,
+    property_object,
+)
--- property_base/models/property_object.py
+++ property_base/models/property_object.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
 
 
-class PropertyRoom(models.AbstractModel):
+class PropertyObject(models.Model):
     _name = "property.object"
     _description = "Common Class for Property Object"
 
     name = fields.Char(
         string="Property",
         required=True,
-        )
+    )
     note = fields.Text(
         string="Notes"
     )
     active = fields.Boolean(
         string="Active",
         default=True
+    )
+    parent_id = fields.Many2one(
+        string="Parent Property",
+        comodel_name="property.object",
+    )
+    partner_id = fields.Many2one(
+        string="Owner",
+        comodel_name="res.partner",
+    )
+    type_id = fields.Many2one(
+        string="Type",
+        comodel_name="property.object_type",
     )
