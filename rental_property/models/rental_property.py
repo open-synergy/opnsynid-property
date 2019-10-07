f92ad4627fb463532cb3be997a355b42c1174a89
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-08 05:35:28 +0700

    [8.0.1.1.0][rental_property]

--- rental_property/models/rental_property.py
+++ rental_property/models/rental_property.py
@@ -2,8 +2,7 @@
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, _
-from openerp.exceptions import Warning as UserError
+from openerp import models, fields, api
 from datetime import datetime
 
 
